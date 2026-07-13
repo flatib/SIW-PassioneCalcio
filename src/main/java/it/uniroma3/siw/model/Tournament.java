a7759e9c94a09afa38a8f8cecff1a1a6681548d8
flatib <fla.tiberia@stud.uniroma3.it>
2026-07-13 22:39:01 +0200

    Update Tournament.java

--- src/main/java/it/uniroma3/siw/model/Tournament.java
+++ src/main/java/it/uniroma3/siw/model/Tournament.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
